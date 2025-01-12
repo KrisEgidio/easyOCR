5819e12efada422b836627394cac0a067da0b137
KrisEgidio <krisegidio@gmail.com>
2025-01-12 19:15:16 -0300

    first

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import re
 
 # Carregar a imagem
-image_path = 'C:\\Users\\graci\\Pictures\\BlueStacks\\Screenshot_2025.01.12_12.26.13.453.png'  # Substitua pelo caminho da sua imagem
+image_path = 'imagem.png'
 img = cv2.imread(image_path)
 
 # Converter para escala de cinza
@@ -13,18 +13,18 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 _, threshold_img = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
 # Inicialize o leitor EasyOCR
-reader = easyocr.Reader(['en'], gpu=False)  # 'en' é o código para o idioma inglês
+reader = easyocr.Reader(['en'], gpu=False)
 
-# Usar EasyOCR para detectar o texto na imagem
+# Detectar o texto na imagem
 results = reader.readtext(threshold_img)
 
-# Variável para armazenar os números encontrados
+
 numbers = ""
 
 # Filtrar e extrair apenas os números
 for result in results:
     text = result[1]
-    numbers += ''.join(re.findall(r'\d', text))  # Pega apenas os dígitos numéricos
+    numbers += ''.join(re.findall(r'\d', text))
 
-# Exibir os números encontrados
+
 print(f"Números encontrados na imagem: {numbers}")
